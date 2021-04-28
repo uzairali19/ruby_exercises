a45c4daaffeeb323397e316602d0a35b068bffc0
Uzair <uzairali19b@gmail.com>
2021-04-28 14:39:34 +0500

    Enumerable

--- ruby_basics/9_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/9_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -34,7 +34,10 @@ def find_word_lengths(word_list)
   # return a hash with each word as the key and its length as the value
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
 
-  word_list.reduce(Hash.new(0)) do |word,result|
-    puts "#{word},#{result}"
-  end
+  votes = ["Bob's Dirty Burger Shack", "St. Mark's Bistro", "Bob's Dirty Burger Shack"]
+
+votes.reduce(Hash.new(0)) do |result, vote|
+  result[vote] += 1
+  result
+end
 end
